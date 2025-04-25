90786f703e745341c2d673ec0c503bbf4225ac8a
Jeong Juyoung <154944675+Ju0s@users.noreply.github.com>
2025-04-25 14:14:27 +0900

    Update file

--- 진료과별 총 예약 횟수 출력하기.sql
+++ 진료과별 총 예약 횟수 출력하기.sql
@@ -1,7 +1,7 @@
 -- 환자 수를 진료과코드 별로 조회
 SELECT
-    MCDP_CD AS '진료과코드',
-    COUNT(*) AS '5월예약건수'
+    MCDP_CD AS 진료과코드,
+    COUNT(*) AS 5월예약건수
 FROM APPOINTMENT
 -- 2022년 5월
 WHERE DATE_FORMAT(APNT_YMD, '%Y-%m') = '2022-05'
